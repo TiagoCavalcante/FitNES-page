b318d0976111baf07cb615e70e424efcf3974011
Pedro Neves <pedro.vasconcelosneves@gmail.com>
2024-11-26 23:34:15 -0300

    Add all files

---
+++ src/main.js
@@ -0,0 +1,5 @@
+AOS.init({
+  duration: 10000, // Duração da animação em milissegundos
+  offset: 200,    // Distância para iniciar a animação
+  once: false,     // Executar a animação apenas uma vez
+});
